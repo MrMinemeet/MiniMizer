3d03b20e9f7bab7dab7f96651274f07a1b9a8a8e
MrMinemeet <alex@wtf-my-code.works>
2024-07-19 13:47:55 +0200

    Add Error on incorrect argument amount
    Also exclude build from git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) { // Assumption the path to the input file is correct
+	if (args.isEmpty()) {
+		throw Error("One argument for the input file expected")
+	} else if (args.size > 1) {
+		System.err.println("More than one argument received, only considering the first one!")
+	}
 
 	// Create a new scanner and parser object with the input file
-
 	val scanner = Scanner(args[0])
 	val parser = Parser(scanner)
 
